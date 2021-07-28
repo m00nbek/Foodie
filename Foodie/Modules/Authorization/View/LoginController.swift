fa2911d65ca12c6d6e0c8a4d461c8cbe0b4a699c
m00nbek <melikulov990@gmail.com>
2021-07-28 18:01:33 +0500

    initial folders created

---
+++ Foodie/Modules/Authorization/View/LoginController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginController.swift
+//  LoginController
+//
+//  Created by Oybek on 7/28/21.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    // MARK: - Properties
+    // MARK: - Selectors
+    // MARK: - API
+    // MARK: - Functions
+}
---
+++ Foodie/Modules/Authorization/View/WelcomeController.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeController.swift
+//  WelcomeController
+//
+//  Created by Oybek on 7/28/21.
+//
+
+import UIKit
+
+class WelcomeController: UIViewController {
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    // MARK: - Properties
+    // MARK: - Selectors
+    // MARK: - API
+    // MARK: - Functions
+}
