471d74e1878838a486fdccd7cf51da1045b64ac8
m00nbek <melikulov990@gmail.com>
2021-07-31 16:31:56 +0500

    added buttons

--- Foodie/Modules/Authorization/View/LoginController.swift
+++ Foodie/Modules/Authorization/View/LoginController.swift
@@ -19,7 +19,41 @@ class LoginController: UIPageViewController {
         let iv = UIImageView()
         iv.image = UIImage(named: "logo")
         iv.contentMode = .scaleAspectFit
+        iv.translatesAutoresizingMaskIntoConstraints = false
         return iv
+    }()
+    private let loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Login", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 16, weight: .medium)
+        button.addTarget(self, action: #selector(loginTapped), for: .touchUpInside)
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    private let signUpButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Sign Up", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 16, weight: .medium)
+        button.addTarget(self, action: #selector(signUpTapped), for: .touchUpInside)
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    private lazy var buttonStack: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [loginButton, signUpButton])
+        stack.axis = .horizontal
+        stack.spacing = 10
+        stack.distribution = .fillEqually
+        
+        stack.clipsToBounds = true
+        stack.layer.cornerRadius = 35
+        stack.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.backgroundColor = .white
+        return stack
     }()
     private lazy var sixtyPercentHeight = (60/100) * view.frame.height
     private let loginView = LoginView()
@@ -41,12 +75,32 @@ class LoginController: UIPageViewController {
     }()
     private lazy var views = [loginView, registerView]
     // MARK: - Selectors
+    @objc private func loginTapped() {
+        
+    }
+    @objc private func signUpTapped() {
+        
+    }
     // MARK: - API
     // MARK: - Functions
     private func configureUI() {
         view.backgroundColor = .white
+        
+        logoImageView.heightAnchor.constraint(equalToConstant: (view.frame.height/4) - 50).isActive = true
+        view.addSubview(logoImageView)
+        logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        logoImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        logoImageView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
+
         scrollView.heightAnchor.constraint(equalToConstant: sixtyPercentHeight).isActive = true
         view.addSubview(scrollView)
+        
+        buttonStack.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        view.addSubview(buttonStack)
+        buttonStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        buttonStack.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        
+        scrollView.topAnchor.constraint(equalTo: buttonStack.bottomAnchor, constant: -30).isActive = true
         scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
