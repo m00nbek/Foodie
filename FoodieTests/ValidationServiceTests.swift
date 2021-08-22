891a27c6c43258b0bd1cc912b590e18db051677b
m00nbek <melikulov990@gmail.com>
2021-08-22 14:26:34 +0500

    Test case added for email

--- FoodieTests/ValidationServiceTests.swift
+++ FoodieTests/ValidationServiceTests.swift
@@ -6,8 +6,23 @@
 //
 
 import XCTest
+@testable import Foodie
 
 class ValidationServiceTests: XCTestCase {
 
-
+    var validation: ValidationService!
+    override func setUp() {
+        super.setUp()
+        validation = ValidationService()
+    }
+    override func tearDown() {
+        super.tearDown()
+        validation = nil
+    }
+    func test_is_valid_email() throws {
+        XCTAssertTrue(validation.validate(.email, for: "some@idk.com"))
+    }
+    func test_is_not_valid_email() throws {
+        XCTAssertFalse(validation.validate(.email, for: "wrongemail"))
+    }
 }
