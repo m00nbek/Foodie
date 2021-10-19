750c663b275874b2777ac759e41997e58365ce19
m00nbek <melikulov990@gmail.com>
2021-10-19 10:26:08 +0500

    removing keyboard notifications

--- Foodie/Modules/Authentication/View/AuthenticationViewController.swift
+++ Foodie/Modules/Authentication/View/AuthenticationViewController.swift
@@ -176,8 +176,7 @@ class AuthenticationViewController: UIViewController, AuthenticationViewProtocol
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     private func removeKeyboardNotifications() {
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.removeObserver(self)
     }
 }
 // MARK: - UIScrollViewDelegate
