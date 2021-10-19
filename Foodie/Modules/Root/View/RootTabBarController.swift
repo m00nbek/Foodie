d175a88e1cbd27b097212ceaef7007deedb78529
m00nbek <melikulov990@gmail.com>
2021-10-19 19:56:54 +0500

    Tabbar icons setup

--- Foodie/Modules/Root/View/RootTabBarController.swift
+++ Foodie/Modules/Root/View/RootTabBarController.swift
@@ -13,10 +13,11 @@ class RootTabBarController: UITabBarController, RootViewProtocol {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        setupControllers()
     }
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        presenter?.presentWelcome(in: self)
+//        presenter?.presentWelcome(in: self)
     }
     // MARK: - Properties
     var presenter: RootPresenterProtocol?
@@ -24,5 +25,25 @@ class RootTabBarController: UITabBarController, RootViewProtocol {
     // MARK: - Functions
     private func setupUI() {
         view.backgroundColor = .white
+        tabBar.backgroundColor = .clear
+    }
+    private func setupControllers() {
+        let home = UINavigationController(rootViewController: HomeRouter.createHome())
+        home.tabBarItem.image = Images.home
+        home.tabBarItem.selectedImage = Images.homeSelected
+        
+        let wishlist = UINavigationController(rootViewController: WishlistRouter.createWishlist())
+        wishlist.tabBarItem.image = Images.wishlist
+        wishlist.tabBarItem.selectedImage = Images.wishlistSelected
+        
+        let orders = UINavigationController(rootViewController: OrdersRouter.createOrders())
+        orders.tabBarItem.image = Images.orders
+        orders.tabBarItem.selectedImage = Images.ordersSelected
+        
+        let profile = UINavigationController(rootViewController: ProfileRouter.createProfile())
+        profile.tabBarItem.image = Images.profile
+        profile.tabBarItem.selectedImage = Images.profileSelected
+        
+        viewControllers = [home, wishlist, orders, profile]
     }
 }
--- Foodie/Utilities/Constants.swift
+++ Foodie/Utilities/Constants.swift
@@ -11,3 +11,14 @@ final class Constants {
     static let mainOrange = UIColor(named: "mainOrange")
     static let gradientOrange = UIColor(named: "gradientOrange")
 }
+
+final class Images {
+    static let home = UIImage(named: "home")
+    static let homeSelected = UIImage(named: "home_selected")
+    static let wishlist = UIImage(named: "wishlist")
+    static let wishlistSelected = UIImage(named: "wishlist_selected")
+    static let orders = UIImage(named: "orders")
+    static let ordersSelected = UIImage(named: "orders_selected")
+    static let profile = UIImage(named: "profile")
+    static let profileSelected = UIImage(named: "profile_selected")
+}
